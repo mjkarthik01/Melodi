0fb825395372a90e838df1b51b74820ffde9e844
KarthikM <mjkarthik2001@gmail.com>
2026-07-17 17:02:06 +0530

    transporter port change

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "://gmai.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
